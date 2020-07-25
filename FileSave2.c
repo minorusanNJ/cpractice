eadd4457544b24593ebb09d23d68eb61ac77d27c
minoruhayata <minoru_hayata@hotmail.com>
2020-07-25 16:23:10 +0900

    Commit for FileSave2.c

--- FileSave2.c
+++ FileSave2.c
@@ -7,6 +7,7 @@ int main(void)
     char    name[5][30];//今回は、２件のデータを書き込むことにします。
     char    age[5][3]; //それで、２次元配列を使います。[データ数][文字の長さ]
     char    sex[5][10]; //今回は、５件まで対応できるようにして、２件で実施
+    char    nat[5][10];
 
 /*
     文字の代入が出来ないので、文字のコピー関数を使います。代入が出来ない理由は、学校で聞いてください、長くなります。strcpy(コピー先,　コピー元）
@@ -21,11 +22,14 @@ int main(void)
     strcpy(age[2],"55");
     strcpy(sex[2],"男性");
 
+    strcpy(nat[1],"日本");
+    strcpy(nat[2],"米国");
+
     pfile = fopen("test2.txt","w"); //ファイルに名前を付けて、書き込みとしてファイルを開く
     
     for(int i=1;i<=2;i++)
     {
-        fprintf(pfile,"%s,%s,%s\n",name[i],age[i],sex[i]); //ファイルに書き込みを行う、名前、年齢、性別の順
+        fprintf(pfile,"%s,%s,%s,%s\n",name[i],age[i],sex[i],nat[i]); //ファイルに書き込みを行う、名前、年齢、性別の順
     }
     
     fclose(pfile); // 書き込み終了後はファイルを閉じる、何を閉じるかはポインターがでわかる
